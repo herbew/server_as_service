7a4561da5df519fc6268d2d4fbd8017b8a21d823
Heribertus Rustyawan <herbew@gmail.com>
2023-04-03 09:29:11 +0700

    add log

--- src/service_listener.py
+++ src/service_listener.py
@@ -1,4 +1,9 @@
 import socket
+import logging, multiprocessing
+
+log = multiprocessing.log_to_stderr()
+log.setLevel(logging.INFO)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('192.168.0.150',1234))
@@ -6,5 +11,5 @@ s.listen(10)
 
 while True:
     client_socket, address = s.accept()
-    print(f'Connection from {address} has been established!')
+    log.info(f'Connection from {address} has been established!')
     client_socket.send(bytes("Wellcome to the server!", "utf-8"))
